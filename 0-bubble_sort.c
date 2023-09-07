2a44a95272fdf82f8c0fc1421dd6975c0b515272
seba17962 <isebsebastiansilva@gmail.com>
2023-09-07 14:18:41 +0000

    fix j for i

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[i] > array[i + 1])
 			{
