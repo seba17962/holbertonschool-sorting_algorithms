d5c47c5b2ba23f697709e5939cc16a873b83d41d
seba17962 <isebsebastiansilva@gmail.com>
2023-09-07 13:20:39 +0000

    add the function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,26 @@
 #include "sort.h"
+
+/**
+ * bubble_sort - the array to sorting
+ * @size: the size of the array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	int i = 0, j = 0;
+	int temp = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+		print_array(array, size);
+	}
+}
