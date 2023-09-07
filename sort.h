889e88f46c367aba9784f91d849e369eace176b6
seba17962 <isebsebastiansilva@gmail.com>
2023-09-07 13:27:58 +0000

    push

--- sort.h
+++ sort.h
@@ -6,48 +6,6 @@
 #include <string.h>
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
-}
-
-/**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
@@ -61,6 +19,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
