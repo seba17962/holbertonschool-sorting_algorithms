137eba888efb822d1d3161c6e6a6a65c915918ea
seba17962 <isebsebastiansilva@gmail.com>
2023-09-07 20:14:05 +0000

    f

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,6 +2,7 @@
 
 /**
  * bubble_sort - the array to sorting
+ * @array: the array to sort
  * @size: the size of the array
 */
 
@@ -10,17 +11,20 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int temp = 0;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 1; i != 0; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (j != size -1)
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-			}
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
+			}	
 		}
-		print_array(array, size);
 	}
 }
