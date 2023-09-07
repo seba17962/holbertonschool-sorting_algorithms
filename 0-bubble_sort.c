0b57cd03ddab87765f7245112b0b55ec8efcaaf8
seba17962 <isebsebastiansilva@gmail.com>
2023-09-07 22:39:09 +0000

    add a reset i = 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,14 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int temp = 0;
 
+	if (array == NULL || size == 0)
+		exit(1);
 	while (i != 0)
 	{
 		i = 0;
 		for (j = 0; j < size; j++)
 		{
-			if (j != size -1)
+			if (j != size - 1)
 			{
 				if (array[j] > array[j + 1])
 				{
