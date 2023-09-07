eaf173b209b6ab46610a41f3c0e61557c85363b3
seba17962 <isebsebastiansilva@gmail.com>
2023-09-07 22:36:24 +0000

    add a reset i = 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int temp = 0;
 
-	for (i = 1; i != 0; i++)
+	while (i != 0)
 	{
 		i = 0;
 		for (j = 0; j < size; j++)
@@ -23,6 +23,7 @@ void bubble_sort(int *array, size_t size)
 					temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
+					i += 1;
 					print_array(array, size);
 				}
 			}	
